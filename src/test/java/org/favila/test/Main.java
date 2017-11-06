ac29265fe1ad72daa1251b2e9ae102a053da567e
favila <favila.ici@gmail.com>
2017-11-05 22:33:24 -0300

    added: -new branch -sample file

---
+++ src/test/java/org/favila/test/Main.java
@@ -0,0 +1,11 @@
+package org.favila.test;
+
+/**
+ * Created by favila on 05-11-17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is not a hello world");
+    }
+}
